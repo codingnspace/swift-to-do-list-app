8fc57adf1412ae8c77f687c441b4fa3415c7a82f
pearl1991 <pearlmcphee@gmail.com>
2019-09-21 16:00:09 -0400

    Initial Commit

---
+++ to-do-list-appTests/to_do_list_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  to_do_list_appTests.swift
+//  to-do-list-appTests
+//
+//  Created by Pearl on 9/21/19.
+//  Copyright © 2019 Pearl. All rights reserved.
+//
+
+import XCTest
+@testable import to_do_list_app
+
+class to_do_list_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
